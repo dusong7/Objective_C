66aac8bc9da9e127a8e362100a93f442f2f6c387
dusong7 <dusong7@sina.com>
2016-01-07 10:49:25 +0800

    Create 4_1.m

---
+++ 4_1.m
@@ -0,0 +1,63 @@
+#import<Foundation/Foundation.h>
+
+@interface XYpoint : NSObject   //interface part
+
+@end
+
+
+
+@implementation XYpoint
+
+@end
+
+
+int main(int argc,const char *argv[])
+{
+//    int _nNum = 100;
+//    float _fNum = 100.109;
+//    double _dNum = 8.44e+11;
+//    char _char = 'W';
+
+
+//    NSLog(@"int is %i\n", _nNum);
+//    NSLog(@"float is %f\n", _fNum);
+//    NSLog(@"double is %e\n", _dNum);
+//    NSLog(@"double is %g\n", _dNum);
+//    NSLog(@"char is %c", _char);
+//(id) newObject: (int) type
+
+//    int a=25, b=5, c=10, d=7;
+//    NSLog(@"a%b is %i", a % b);
+//    NSLog(@"a%%c is %i", a % c); //%c 转义
+//    NSLog(@"a%%d is %i", a % d);  //%d转义
+//    NSLog(@"a%b is %i", a / d * d + a % d);
+
+//    float f1 = 123.125, f2;
+//    int i1, i2 = -150;
+
+//    i1 = f1;
+//    NSLog(@"%f f->i %i",f1, i1);
+
+//    f1 = i2;
+//    NSLog(@"%i i->f %f", i2,f1);
+
+//    f1 = i2 / 100;
+//    NSLog(@"%i div100 %f", i2, f1);
+
+//    f2 = i2 / 100.0;
+//    NSLog(@"%i div100.0 %f", i2, f2);
+
+//    f2 = (float) i2/100;
+//    NSLog(@"(float)%i div100 %f", i2, f2);
+
+    //%g scientific
+
+//    id myNum = 'W';
+//    char _nNum;
+
+//    _nNum = (char) myNum;
+
+//     NSLog(@"%c",_nNum);
+
+  return(0);
+}

9b023e8af1c7d3374d7823e02bcfd4f76a5f2b44
dusong7 <dusong7@sina.com>
2016-01-07 09:21:13 +0800

    Update leran_oc.m

--- leran_oc.m
+++ leran_oc.m
@@ -1,10 +1,11 @@
+
 #import<Foundation/Foundation.h>
 
 @interface Fraction : NSObject   //interface part
 
 {
-	int numerator;
-	int denominator;
+int numerator;
+int denominator;
 }
 - (int) numerator;
 - (int) denominator;
@@ -17,23 +18,18 @@
 @interface Car : NSObject   //interface part
 
 {
- int numerator;
- int denominator;
+int numerator;
+int denominator;
 }
 - (int) numerator;
 - (int) denominator;
 
-Oil
-
-
-
-
-driverDistance
-Oil
-Engine
-FM
-Speed
-Service
+//driverDistance
+//Oil
+//Engine
+//FM
+//Speed
+//Service
 
 
 
@@ -44,112 +40,100 @@ Service
 
 @implementation Fraction
 
- - (void) setNumerator: (int) n
- - (void) setDenominator: (int) d
- @implementation
- @implementation myClass
-{
-  memberDeclarations;
-}
-@end
-
-
-
-
 -(void) print
 {
-	NSLog(@"%i/%i", numerator, denominator);
+NSLog(@"%i/%i", numerator, denominator);
 }
 -(void) setNumerator: (int) n
 {
-	numerator = n;
+numerator = n;
 }
 
 -(void) setDenominator: (int) d
 {
-	denominator = d;
+denominator = d;
 }
 
 -(int) numerator
 {
-	return numerator;
-	   return 1;
-	}
+return numerator;
+return 1;
+}
 
-	-(int) denominator
-	{
-	return denominator;
-	    return 1;
-	}
+-(int) denominator
+{
+return denominator;
+return 1;
+}
 
-	@end
-
-
-		int main(int argc,const char *argv[]){
-
-			    NSLog(@"%@",@"Hello I am OC!");
-			    NSLog(@"Testing ...\n..1\n...2\n....3\n");
-			    int sum;
-			    sum = 50 + 30;
-			    NSLog(@"The sum is %i", sum);
-			    NSLog(@"In Objective-C. lowercase letter are significant\n");
-			    NSLog(@"main is where program execution begin\n");
-			    NSLog(@"Open and close braces enclose program statements in a routime\n");
-			    NSLog(@"All program statements must be terminated by a semicolon\n");
-
-			    int i;
-			    i = 1;
-			    NSLog(@"Testing...");
-			    NSLog(@"....%i", i);
-			    NSLog(@"...%i", i+1);
-			    NSLog(@"..%i",i+2);
-
-			    int iResult = 0;
-			    iResult = 87 - 15;
-			    NSLog(@"The Result of 87 - 15 is %i", iResult);
-			      INT sum;
-			      sum = 25 + 37 - 19;
-			      NSLog(@"The answer is %i", sum);
-			    int answer;
-			    int result;
-
-			    answer = 100;
-			    result = answer - 10;
-			    NSLog(@"The result is %i\n", result + 5);
-
-			    int numerator = 1;
-			    int denominator  = 3;
-			    NSLog(@"The fraction is %i/%i", numerator, denominator);
+@end
 
 
-			-(void) print;
-			-(void) setNumerator: (int) n;
-			-(void) setDenominator: (int) d;
+int main(int argc,const char *argv[]){
 
-			    Fraction *myFraction;
-			   Fraction *mySecFraction = [Fraction new];
-			  myFraction = [[Fraction alloc] init];
-			  mySecFraction = [[Fraction alloc] init];
+	NSLog(@"%@",@"Hello I am OC!");
+	NSLog(@"Testing ...\n..1\n...2\n....3\n");
+	int sum;
+	sum = 50 + 30;
+	NSLog(@"The sum is %i", sum);
+	NSLog(@"In Objective-C. lowercase letter are significant\n");
+	NSLog(@"main is where program execution begin\n");
+	NSLog(@"Open and close braces enclose program statements in a routime\n");
+	NSLog(@"All program statements must be terminated by a semicolon\n");
 
-			myFraction = [Fraction alloc]
-			myFraction = [myFraction init];
+	int i;
+	i = 1;
+	NSLog(@"Testing...");
+	NSLog(@"....%i", i);
+	NSLog(@"...%i", i+1);
+	NSLog(@"..%i",i+2);
 
-			   [myFraction setNumerator: 2];
-			 [myFraction setDenominator: 3];
+	int iResult = 0;
+	iResult = 87 - 15;
+	NSLog(@"The Result of 87 - 15 is %i", iResult);
+	INT sum;
+	sum = 25 + 37 - 19;
+	NSLog(@"The answer is %i", sum);
+	int answer;
+	int result;
 
-			    [mySecFraction setNumerator: 3];
-			  [mySecFraction setDenominator: 7];
+	answer = 100;
+	result = answer - 10;
+	NSLog(@"The result is %i\n", result + 5);
+
+	int numerator = 1;
+	int denominator  = 3;
+	NSLog(@"The fraction is %i/%i", numerator, denominator);
 
 
-			    NSLog(@"The value of myFraction is:" );
-			  [myFraction print];
-			[instance method] //实例方法 //类方法
+	//-(void) print;
+	//-(void) setNumerator: (int) n;
+	//-(void) setDenominator: (int) d;
 
-			NSLog(@"The value of mySecFraction is:" );
-			[mySecFraction print];
+	Fraction *myFraction;
+	Fraction *mySecFraction = [Fraction new];
+	myFraction = [[Fraction alloc] init];
+	mySecFraction = [[Fraction alloc] init];
 
-			NSLog(@"The value is %i/%i",
-				[myFraction numerator],[myFraction denominator]);
+	myFraction = [Fraction alloc]
+	myFraction = [myFraction init];
 
-			return(0);
-	}
+	[myFraction setNumerator: 2];
+	[myFraction setDenominator: 3];
+
+	[mySecFraction setNumerator: 3];
+	[mySecFraction setDenominator: 7];
+
+
+	NSLog(@"The value of myFraction is:" );
+	[myFraction print];
+//	[instance method] //实例方法 //类方法
+
+	NSLog(@"The value of mySecFraction is:" );
+	[mySecFraction print];
+
+	NSLog(@"The value is %i/%i",
+	[myFraction numerator],[myFraction denominator]);
+
+return(0);
+}

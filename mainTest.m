36218a7f91f7f9859583c6a1322e59e39d7fb568
dusong7 <dusong7@hotmail.com>
2016-08-07 22:46:03 +0800

    Update mainTest.m

--- mainTest.m
+++ mainTest.m
@@ -13,6 +13,8 @@
 
 @synthesize numberN, numberM;
 
+int xValue = 100;
+
 -(void) show
 {
     NSLog(@"The value is M %i, N %i", numberN, numberM);
@@ -25,9 +27,21 @@
     numberN += numberM;
 }
 
--(void) multi: (int)m
+-(void) multi: (TestCalss *)m
 {
-    numberM = m;
+    numberM = numberM * m.numberM + numberN * m.numberN;
+    numberN = numberN * m.numberN + numberM * m.numberN;
+}
+
+@end
+
+@implementation TestClsBT
+
+-(void) showMore
+{
+    NSLog(@"Show more %i", xValue);
+    [self show];
+
 }
 
 @end
@@ -41,13 +55,25 @@ int main(int argc, const char * argv[]) {
 
         TestCalss *testcls;
 
+        TestClsBT *testBt = [[TestClsBT alloc] init];
+
         testcls = [TestCalss alloc];
         testcls = [testcls init];
 
-        [testcls add:3 over:9];
-        [testcls multi:8];
-
+        [testcls add:10 over:10];
         [testcls show];
+
+        [testcls multi:testcls];
+        [testcls show];
+
+        [testBt add:10 over:10];
+        [testBt showMore];
+//
+//        2016-08-07 22:45:04.697 cmdLearn[1339:39432] The value is M 20, N 10
+//        2016-08-07 22:45:04.698 cmdLearn[1339:39432] The value is M 10400, N 500
+//        2016-08-07 22:45:04.698 cmdLearn[1339:39432] Show more 100
+//        2016-08-07 22:45:04.698 cmdLearn[1339:39432] The value is M 20, N 10
+//        Program ended with exit code: 0
 
 //        int nValue = 0;
 //        scanf("%i",&nValue); //keyborad input, alike C language

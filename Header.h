0164286430b92104de373ed4599ef480b61d65bb
dusong7 <dusong7@hotmail.com>
2016-08-07 22:08:26 +0800

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,24 @@
+//
+//  Header.h
+//  cmdLearn
+//
+//  Created by apple on 16/8/7.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
+
+#import <Foundation/Foundation.h>
+
+@interface TestCalss : NSObject
+
+@property int numberN, numberM;
+
+-(void) show;
+-(void) add: (int)n over: (int)m;
+-(void) multi:(int)m;
+@end
